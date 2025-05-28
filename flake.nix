2b81677d1a5df3e51086e46b15794f551710cc8d
Austin Herron <austinh@consumerdirectcare.com>
2025-05-28 08:33:49 -0600

    Use go 1.24.2

--- flake.nix
+++ flake.nix
@@ -9,11 +9,18 @@
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+    go1_24_2 = pkgs.go.overrideAttrs (oldAttrs: {
+      version = "1.24.2";
+      src = pkgs.fetchurl {
+        url = "https://go.dev/dl/go1.24.2.src.tar.gz";
+        hash = "sha256-ncd/+twW2DehvzLZnGJMtN8GR87nsRnt2eexvMBfLgA=";
+      };
+    });
     nvimAppName = "mynvim";
   in {
     devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-            go
+            go1_24_2
             gopls
             gofumpt
             golangci-lint
@@ -38,6 +45,8 @@
             
             rsync -av --delete "${self}/nvim/" "$HOME/.config/$NVIM_APPNAME/"
             chmod -R u+rw "$HOME/.config/$NVIM_APPNAME/"
+
+            zsh
         '';
 
     };
